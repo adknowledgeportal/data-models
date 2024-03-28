f458a27afddd754cd72a353ce79c82fcc74ff740
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2024-03-27 17:12:19 -0700

    change path

--- tests/changed_manifest_selection.sh
+++ tests/changed_manifest_selection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ./scripts/identify_changed_manifests_for_testing.py \
+python identify_changed_manifests_for_testing.py \
     --changed_files_path ./tests/changed-files.txt \
     --current_templates_url https://raw.githubusercontent.com/adknowledgeportal/data-models/main/modules/template/templates.csv \
     --new_templates_path ./modules/template/templates.csv \
