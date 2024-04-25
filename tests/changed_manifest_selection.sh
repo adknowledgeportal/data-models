00476efd1548767d3c4ba6274e2e2510b5e3f25e
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2024-04-25 09:40:51 -0700

    fix module directory

--- tests/changed_manifest_selection.sh
+++ tests/changed_manifest_selection.sh
@@ -3,7 +3,7 @@
 python identify_changed_manifests_for_testing.py \
     --changed_files_path changed-files.txt \
     --current_templates_url https://raw.githubusercontent.com/adknowledgeportal/data-models/main/modules/template/templates.csv \
-    --new_templates_path ./modules/template/templates.csv \
+    --new_templates_path ../modules/template/templates.csv \
     --csv_model_path ../AD.model.csv \
     --template_config_path ../dca-template-config.json \
     --output_file_path changed-templates.json
