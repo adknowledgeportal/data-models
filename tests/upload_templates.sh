447e57ac1021d3236df1d47e6ac8566cc26fc09a
GiaJordan <gianna.jordan@sagebase.org>
2024-12-17 13:51:53 -0700

    add script to upload templates

---
+++ tests/upload_templates.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# upload metadata templates to synapse
+# if running locally set SYNAPSE_TOKEN_DPE in your environment
+
+SYNAPSE_UPLOAD_FOLDER_ID=syn64416308
+
+
+if [ -n "${SYNAPSE_TOKEN_DPE}" ]; then
+  echo "Using synapse token from env var"
+  synapse login -p "${SYNAPSE_TOKEN_DPE}"
+else
+  echo No credentials found
+fi
+
+echo Uploading manifests to "$SYNAPSE_UPLOAD_FOLDER_ID"
+
+shopt -s extglob
+
+MANIFESTS=("$PWD"/*.@(xlsx|xls))
+
+for MANIFEST in ${MANIFESTS[@]};
+do
+  synapse store --parentId "$SYNAPSE_UPLOAD_FOLDER_ID" --noForceVersion "$MANIFEST"
+done
+
+echo "✓ Done!"
