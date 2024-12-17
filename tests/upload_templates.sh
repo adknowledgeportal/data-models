008f522ed836cdc74cadb64699ec8934b2ec713c
GiaJordan <gianna.jordan@sagebase.org>
2024-12-17 14:40:29 -0700

    remove login step

--- tests/upload_templates.sh
+++ tests/upload_templates.sh
@@ -2,15 +2,8 @@
 # upload metadata templates to synapse
 # if running locally set SYNAPSE_TOKEN_DPE in your environment
 
+
 SYNAPSE_UPLOAD_FOLDER_ID=syn64416308
-
-
-if [ -n "${SYNAPSE_TOKEN}" ]; then
-  echo "Using synapse token from env var"
-  synapse login -p "${SYNAPSE_TOKEN}"
-else
-  echo No credentials found
-fi
 
 echo Uploading manifests to "$SYNAPSE_UPLOAD_FOLDER_ID"
 
