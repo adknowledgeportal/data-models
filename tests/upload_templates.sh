6b4843c8246bbd96d0d815340ba338ecc0d00d96
GiaJordan <gianna.jordan@sagebase.org>
2024-12-18 10:33:02 -0700

    use env var, reorg

--- tests/upload_templates.sh
+++ tests/upload_templates.sh
@@ -3,19 +3,16 @@
 # if running locally set SYNAPSE_TOKEN_DPE in your environment
 
 set -e
-
-SYNAPSE_UPLOAD_FOLDER_ID=syn64416621
-
-echo Uploading manifests to "$SYNAPSE_UPLOAD_FOLDER_ID"
-
 shopt -s extglob
+
+echo Uploading manifests to "${SYNAPSE_UPLOAD_FOLDER_ID}"
 
 MANIFESTS=("$PWD"/*.@(xlsx|xls))
 
 for MANIFEST in ${MANIFESTS[@]};
 do
   echo "Uploading $MANIFEST"
-  synapse store --parentId "$SYNAPSE_UPLOAD_FOLDER_ID" --noForceVersion "$MANIFEST"
+  synapse store --parentId "${SYNAPSE_UPLOAD_FOLDER_ID}" --noForceVersion "$MANIFEST"
 done
 
 echo "✓ Done!"
