4fbf10e5731d00af3148f97215da0c4c70d970b3
GiaJordan <gianna.jordan@sagebase.org>
2025-08-21 11:38:58 -0700

    reorganize org gathering

--- scripts/upload_jsonschema.py
+++ scripts/upload_jsonschema.py
@@ -5,6 +5,7 @@ from json import load
 from importlib_metadata import files
 
 import synapseclient
+from synapseclient.core.exceptions import SynapseHTTPError
 
 # Environment Variables are set in the github workflow
 ORG_NAME = os.environ.get("SYNAPSE_ORGANIZATION")
@@ -20,39 +21,41 @@ def import_json_schema(filename: str) -> dict:
 
     return schema
 
-def create_org(syn, js,) -> None:
+def get_org(js,) -> None:
     """
-    Check if the given organization exists on synapse, and create it if it doesn't.
+    Create or get the JsonSchemaOrganization for the given organization name.
     """
-    all_orgs = js.list_organizations()
-    for org in all_orgs:
-        if org["name"] == ORG_NAME:
-            syn.logger.info(f"Organization {ORG_NAME} already exists.")
-            break
-    else:
-        syn.logger.info(f"Creating organization {ORG_NAME}.")
-        js.create_organization(ORG_NAME)
+    try:
+        json_schema_org = js.create_organization(ORG_NAME)
+    except SynapseHTTPError as e:
+        if e.response.status_code == 400 and "already exists" in e.response.text:
+            print(f"Organization {ORG_NAME} already exists.")
+            json_schema_org = js.get_organization(ORG_NAME)
+        else:
+            raise e
 
-def register_schema(schema: dict, filename: str,) -> None:
+    return json_schema_org
+
+def register_schema(schema: dict, filename: str, json_schema_org) -> None:
     """
-    Register the given JsonSchema on synapse under the organization provided
+    Register the given JsonSchema on synapse under the organization provided.
     """
-    syn = synapseclient.Synapse()
-    syn.login()
-
-    js = syn.service("json_schema")
-    create_org(syn, js)
-
-    json_schema_org = js.JsonSchemaOrganization(name=ORG_NAME)
-
-    json_schema_org.get()
 
     uri = Path(filename).stem
     json_schema_org.create_json_schema(schema, uri, VERSION)
 
+
+
+# Log in to synapse
+syn = synapseclient.Synapse()
+syn.login()
+
+# Get the relevant org from synapse
+js = syn.service("json_schema")
+json_schema_org = get_org(js)
+
 # Iterate through directory with jsonschema files and register each one
 files = glob.glob("*.json")
-
 for file in files:
     schema = import_json_schema(file)
-    register_schema(schema, file)
+    register_schema(schema=schema, file=file, json_schema_org=json_schema_org)
