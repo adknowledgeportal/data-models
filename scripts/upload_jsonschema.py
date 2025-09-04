888063954b45e167ef0acd6f9f87e2b0af82bdfe
GiaJordan <gianna.jordan@sagebase.org>
2025-09-04 11:11:24 -0700

    change to calls for older client version

--- scripts/upload_jsonschema.py
+++ scripts/upload_jsonschema.py
@@ -26,12 +26,18 @@ def get_org(js: JsonSchemaService) -> JsonSchemaOrganization:
     """
     Create or get the JsonSchemaOrganization for the given organization name.
     """
+    print("Getting org")
     try:
-        json_schema_org = js.create_organization(ORG_NAME)
+        print("creating org")
+        json_schema_org = js.JsonSchemaOrganization(ORG_NAME)
+        json_schema_org.create()
+        print("org created")
     except SynapseHTTPError as e:
+        print("org dne")
         if e.response.status_code == 400 and "already exists" in e.response.text:
             print(f"Organization {ORG_NAME} already exists.")
-            json_schema_org = js.get_organization(ORG_NAME)
+            json_schema_org = js.JsonSchemaOrganization(ORG_NAME)
+            print("org retrieved")
         else:
             raise e
 
@@ -41,6 +47,8 @@ def get_org(js: JsonSchemaService) -> JsonSchemaOrganization:
 # Log in to synapse
 syn = synapseclient.Synapse()
 syn.login()
+syn.get_available_services()
+
 
 # Get the relevant org from synapse
 js = syn.service("json_schema")
@@ -52,7 +60,9 @@ for file in files:
     schema = import_json_schema(file)
     schema_name = Path(file).stem
     try:
+        print("creating schema")
         json_schema_org.create_json_schema(schema, schema_name, VERSION)
+        print("schema created")
     except SynapseHTTPError as e:
                 if str(e.args[0]).endswith("already exists for this JSON schema"):
                     continue
