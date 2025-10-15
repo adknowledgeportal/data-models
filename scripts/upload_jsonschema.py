a4344ecf24720f9956fc5500d1545ade332a7286
SageGJ <gianna.jordan@sagebase.org>
2025-10-15 12:22:06 -0700

    update schema naming and version convention (#223)
    * update schema naming and version convention
    
    * rm print statement
    
    * rm validation from name
    
    * update error handling

--- scripts/upload_jsonschema.py
+++ scripts/upload_jsonschema.py
@@ -52,11 +52,14 @@ files = glob.glob("*.json")
 for i, file in enumerate(files):
     schema = import_json_schema(file)
     schema_name = Path(file).stem
-    schema_name = schema_name.replace("_", ".").replace("-", ".")
+    schema_name = "ad." + schema_name.replace("_", ".").replace("-", ".").replace("validation.","")
     print(f"Uploading schema {i+1}/{len(files)}: {schema_name}")
+
     try:
-        json_schema_org.create_json_schema(schema, schema_name, VERSION)
+        json_schema_org.create_json_schema(schema, schema_name, VERSION.replace("v", ""))
     except SynapseHTTPError as e:
-                if str(e.args[0]).endswith("already exists for this JSON schema"):
-                    continue
-                raise e
+        version_already_exists_message = f"Semantic version: '{VERSION.replace('v', '')}' already exists for this JSON schema"
+        if e.response.status_code == 400 and version_already_exists_message in e.response.text:
+            print(f"Version {VERSION} already exists for schema {schema_name}, skipping...")
+        else:
+            raise e
